257cad4ff3f3511552cbccb7e2f93d8284d40bd2
Jens Klingenberg <mail@jensklingenberg.de>
2021-06-06 11:50:42 +0200

    Change example code

--- CabretDemo/shared/src/commonMain/kotlin/de/jensklingenberg/cabret/cabretdemo/shared/Greeting.kt
+++ CabretDemo/shared/src/commonMain/kotlin/de/jensklingenberg/cabret/cabretdemo/shared/Greeting.kt
@@ -2,20 +2,21 @@ package de.jensklingenberg.cabret.cabretdemo.shared
 
 import de.jensklingenberg.cabret.Cabret
 import de.jensklingenberg.cabret.DebugLog
-
+import de.jensklingenberg.cabret.LogData
 
 
 class Greeting {
 
     fun greeting(greetingText: String = "Hello"): String {
+
         /*
         Cabret.addLogger(object : Cabret.Logger {
-
             override fun log(data: LogData) {
+                //Add your logger here
                 println(data.tag + " " + data.msg)
             }
         })
-        */
+*/
         return realgreeting(greetingText)
     }
 
